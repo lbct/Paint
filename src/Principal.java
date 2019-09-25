03b1dfae444e24fec04d790531366d38e3d098bf
lbct <lbct95@gmail.com>
2019-09-25 19:13:38 -0400

    test

---
+++ src/Principal.java
@@ -0,0 +1,11 @@
+import vista.PanelDibujo;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PanelDibujo panel = new PanelDibujo();
+		panel.setVisible(true);
+	}
+
+}
